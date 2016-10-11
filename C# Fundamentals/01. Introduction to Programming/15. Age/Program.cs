90f92a27a3ec6e4c3816b207a833d02d7665b354
Elisaveta Todorova <eli961@abv.bg>
2016-10-11 19:06:50 +0300

    Fixing task 15 Age.

--- C# Fundamentals/01. Introduction to Programming/15. Age/Program.cs
+++ C# Fundamentals/01. Introduction to Programming/15. Age/Program.cs
@@ -11,8 +11,13 @@ namespace _15.Age
         //Fix it later
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            string[] param = input.Split('.');
+            DateTime userBirthday = DateTime.Parse(Console.ReadLine());
+            DateTime timeToday = DateTime.Today;
+            long period = timeToday.Subtract(userBirthday).Ticks;
+            int years = new DateTime(period).Year - 1;
+            Console.WriteLine(years);
+            int futureYears = years + 10;
+            Console.WriteLine(futureYears);
 
         }
     }
