c043fce07bde5f8cea85e4e41723de1fa218cbd2
Elisaveta Todorova <eli961@abv.bg>
2016-10-17 16:05:07 +0300

    Adding task 14 and 15.

---
+++ C# Fundamentals/03. Operators and Expressions/14.BitExchange/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _14.BitExchange
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var n = uint.Parse(Console.ReadLine());
+            uint mask = 1;
+
+            var third = (n & (mask << 3)) >> 3;
+            var forth = (n & (mask << 4)) >> 4;
+            var fifth = (n & (mask << 5)) >> 5;
+
+            var twenyFourt = (n & (mask << 24)) >> 24;
+            var twenyFifth = (n & (mask << 25)) >> 25;
+            var twenySixth = (n & (mask << 26)) >> 26;
+
+            // 0 - n & ~(mask << p)
+            // 1 - (n | mask << p)
+
+            n = third == 0 ? n & ~(mask << 24) : (n | mask << 24);
+            n = forth == 0 ? n & ~(mask << 25) : (n | mask << 25);
+            n = fifth == 0 ? n & ~(mask << 26) : (n | mask << 26);
+            n = twenyFourt == 0 ? n & ~(mask << 3) : (n | mask << 3);
+            n = twenyFifth == 0 ? n & ~(mask << 4) : (n | mask << 4);
+            n = twenySixth == 0 ? n & ~(mask << 5) : (n | mask << 5);
+
+            Console.WriteLine(n);
+           
+        }
+    }
+}
---
+++ C# Fundamentals/03. Operators and Expressions/15.BitSwap/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace _15.BitSwap
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var n = uint.Parse(Console.ReadLine());
+            var p = int.Parse(Console.ReadLine());
+            var q = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+            uint mask = 1;
+            for (int i = 0; i < k; i++)
+            {
+                var currentPBit = (n & (mask << p)) >> p;
+                var currentQBit = (n & (mask << q)) >> q;
+                
+                if (currentPBit == 0)
+                {
+                    n &= ~(mask << q);
+                }
+                else
+                {
+                    n |= (n | mask << q);
+                }
+
+                if (currentQBit == 0)
+                {
+                    n &= ~(mask << p);
+                }
+                else
+                {
+                    n = (n | mask << p);
+                }
+
+                p++;
+                q++;
+            }
+            Console.WriteLine(n);
+
+        }
+    }
+}
