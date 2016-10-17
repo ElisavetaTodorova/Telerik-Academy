eb3cee72c96a0136cd47204187d454683d7131ed
Elisaveta Todorova <eli961@abv.bg>
2016-10-17 13:24:14 +0300

    Adding task  7 and 8.

---
+++ C# Fundamentals/03. Operators and Expressions/07.PointInCircle/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07.PointInCircle
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var x = double.Parse(Console.ReadLine());
+            var y = double.Parse(Console.ReadLine());
+
+            var result = Math.Sqrt(x * x + y * y);
+
+            if (result <= 2)
+            {
+                Console.WriteLine("yes {0:F2}", result);
+            }
+            else
+            {
+                Console.WriteLine("no {0:F2}", result);
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/03. Operators and Expressions/08.PrimeCheck/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08.PrimeCheck
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var isPrime = true;
+
+            var sqrtValue = Math.Sqrt(n);
+
+            for (int i = 2; i <= sqrtValue; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine(isPrime && n > 1 ? "true" : "false");
+        }
+    }
+}
