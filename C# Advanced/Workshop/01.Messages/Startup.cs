1a3c189d92902f25613e6118aa5c0686fb565aa2
Elisaveta Todorova <eli961@abv.bg>
2016-11-27 22:09:59 +0200

    Adding solutions for the tasks from Wednesday's Workshop.

---
+++ C# Advanced/Workshop/01.Messages/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _01.Messages
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var input1 = Console.ReadLine();
+            var operation = Console.ReadLine();
+            var input2 = Console.ReadLine();
+
+            var codes = new string[] {"cad",
+                                      "xoz",
+                                      "nop",
+                                      "cyk",
+                                      "min",
+                                      "mar",
+                                      "kon",
+                                      "iva",
+                                      "ogi",
+                                      "yan" };
+
+            var input1Codes = new List<string>();
+            var input2Codes = new List<string>();
+
+            int input1StartIndex = 0;
+            int input2StartIndex = 0;
+
+            while (input1StartIndex < input1.Length)
+            {
+                input1Codes.Add(input1.Substring(input1StartIndex, 3));
+                input1StartIndex += 3;
+
+            }
+
+            while (input2StartIndex < input2.Length)
+            {
+                input2Codes.Add(input2.Substring(input2StartIndex, 3));
+                input2StartIndex += 3;
+
+            }
+
+            var input1Number = new StringBuilder();
+            var input2Number = new StringBuilder();
+
+            foreach (var item in input1Codes)
+            {
+                input1Number.Append(Array.IndexOf(codes, item));
+            }
+
+            foreach (var item in input2Codes)
+            {
+                input2Number.Append(Array.IndexOf(codes, item));
+            }
+
+            var firstNumber = BigInteger.Parse(input1Number.ToString());
+            var seconNumber = BigInteger.Parse(input2Number.ToString());
+
+            BigInteger numberResult = 0;
+
+            if (operation == "+")
+            {
+                numberResult = firstNumber + seconNumber;
+            }
+            else
+            {
+                numberResult = firstNumber - seconNumber;
+            }
+
+            var numberResultAsString = numberResult + "";
+
+            var finalString = new StringBuilder();
+
+            foreach (var item in numberResultAsString)
+            {
+                var str = codes[item - 48];
+
+                finalString.Append(str);
+            }
+
+            Console.WriteLine(finalString.ToString());
+
+        }
+    }
+}
---
+++ C# Advanced/Workshop/02.BitShiftMatrix/Startup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.BitShiftMatrix
+{
+    class Startup
+    {
+        static void Main()
+        {
+            int matrixRow = int.Parse(Console.ReadLine());
+            int matrixCol = int.Parse(Console.ReadLine());
+            int numberOfMoves = int.Parse(Console.ReadLine());
+
+            var moves = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var matrix = new int[matrixRow, matrixCol];
+            FillMatrix(matrix, matrixRow, matrixCol);
+
+            var coef = Math.Max(matrixCol, matrixRow);
+
+            var strartRow = matrixRow - 1;
+            var startCol = 0;
+
+            long sum = 0;
+
+            foreach (var move in moves)
+            {
+                int currentRow = move / coef;
+                int currentCol = move % coef;
+
+                if (currentCol > startCol)
+                {
+                    for (int i = startCol; i <= currentCol; i++)
+                    {
+                        sum += matrix[strartRow, i];
+                        matrix[strartRow, i] = 0;
+                    }
+                }
+                else
+                {
+                    for (int i = startCol; i >= currentCol; i--)
+                    {
+                        sum += matrix[strartRow, i];
+                        matrix[strartRow, i] = 0;
+                    }
+                }
+
+                if (currentRow > strartRow)
+                {
+                    for (int i = strartRow; i <= currentRow; i++)
+                    {
+                        sum += matrix[i, currentCol];
+                        matrix[i, currentCol] = 0;
+                    }
+                }
+                else
+                {
+                    for (int i = strartRow; i >= currentRow; i--)
+                    {
+                        sum += matrix[i, currentCol];
+                        matrix[i, currentCol] = 0;
+                    }
+                }
+
+                strartRow = currentRow;
+                startCol = currentCol;
+            }
+
+            Console.WriteLine(sum);
+
+
+        }
+
+        private static void FillMatrix(int[,] matrix, int row, int col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = 1 << (row - 1 - i + j);
+                }
+            }
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
