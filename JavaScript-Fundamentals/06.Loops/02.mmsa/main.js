ff16aed9622c03a35f612f09b1b540399cdc9796
Elisaveta Todorova <Elisaveta Todorova>
2016-12-18 16:55:26 +0200

    Fixing task 2

--- JavaScript-Fundamentals/06.Loops/02.mmsa/main.js
+++ JavaScript-Fundamentals/06.Loops/02.mmsa/main.js
@@ -1,21 +1,26 @@
 function solve(params) {
-    let a = +params[0];
-    let b = +params[1];
-    let c = +params[2];
+    var min = +params[0];
+    var max = +params[0];
+    var sum = 0;
+    var avarage = 0;
+    
+    for (var i = 0; i < params.length; i++) {
+        var element = +params[i];
+        if (element > max) {
+            max = element;
+        }
 
+        if (element < min) {
+            min = element;
+        }
 
-    let min = Math.min(a ,b ,c);
-    let max = Math.max(a, b, c);
-    let sum = a + b + c;
-    let avrg = sum / 3;
-    // min=1.00
-    // max=5.00
-    // sum=8.00
-    // avg=2.67
-    console.log(`min=${min.toFixed(2)}`);
-    console.log(`max=${max.toFixed(2)}`);
-    console.log(`sum=${sum.toFixed(2)}`);
-    console.log(`avrg=${avrg.toFixed(2)}`);
+        sum+=element;
+    }
+    avarage = sum / params.length;
+    console.log('min='+min.toFixed(2));
+    console.log('max='+max.toFixed(2));
+    console.log('sum='+sum.toFixed(2));
+    console.log('avg='+avarage.toFixed(2));
 }
 
 solve(['2', '5', '1']);
